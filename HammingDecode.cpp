517efcf38db78dccc0ba2419d6eca9bf77187b26
Zach Hamby <150568022+zhamby@users.noreply.github.com>
2024-12-03 23:01:58 +0000

    init pseudo code

---
+++ HammingDecode.cpp
@@ -0,0 +1,4 @@
+// Read each line of a binary text file by 7 bits (ints) at a time
+// Check the 7 bit's parity for errors; error correct as needed.
+// Error message if too many errors in a block
+// output decoded message to created file
