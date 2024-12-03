495cef567a24a451e0c5686953923354f952fa65
Zach Hamby <150568022+zhamby@users.noreply.github.com>
2024-12-03 22:08:01 +0000

    init main testing framework

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Hamming.h"
+
+int main() {
+    // Vector to store Hamming objects
+    std::vector<Hamming> hammingObjects;
+
+    // Populate 15 filenames and create Hamming objects
+    for (int i = 1; i <= 15; ++i) {
+        std::string filename = "test" + std::to_string(i) + ".txt";
+        hammingObjects.emplace_back(filename); // Construct Hamming object with the filename
+    }
+
+    //Hamming object testing
+    for (const auto& hamming : hammingObjects) {
+        hamming.print(); //testing
+    }
+
+    return 0;
+}
