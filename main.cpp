872566e77c4d31d8b61b4dad81afcd15ed4a4347
ctcriswe <ctcriswe@uab.edu>
2024-12-03 22:34:14 +0000

    Made the generator and parity check matricies

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main() {
     // Vector to store Hamming objects
     std::vector<Hamming> hammingObjects;
 
+/*
     // Populate 15 filenames and create Hamming objects
     for (int i = 1; i <= 15; ++i) {
         std::string filename = "test" + std::to_string(i) + ".txt";
@@ -17,6 +18,9 @@ int main() {
     for (const auto& hamming : hammingObjects) {
         hamming.print(); //testing
     }
+*/
+    Hamming hammingCode;
+    hammingCode.printOut();
 
     return 0;
 }
