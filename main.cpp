8098c5720a8d06c77c8a6d88eb6a51619e24d383
ctcriswe <ctcriswe@uab.edu>
2024-12-07 05:04:02 +0000

    added the other test files to main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "Eigen/Dense"
 
 int main() {
-    for (int i = 1; i <= 2; ++i) {
+    for (int i = 1; i <= 5; ++i) {
         // Generate Filename
         std::string fileName = "test" + std::to_string(i);
         
@@ -18,10 +18,10 @@ int main() {
         std::cout << "_____ 2. Start Error Encoder _____\n";
         ErrorEncode errorEncode2(fileName + ".txt"); //also calls Encode processFile()
 
-        std::cout << "________ 3. Start Decoder ________\n";
+        std::cout << "________ 3. Start Decoder(No Errors) ________\n";
         Decode decode3(fileName + "_out.txt");
         
-        std::cout << "____ 4. Start Decoder (ERROR) ____\n";
+        std::cout << "____ 4. Start Decoder (Errors) ____\n";
         Decode decode4(fileName + "_e_out.txt");
     }
     return 0;
